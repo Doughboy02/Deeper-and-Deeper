9ba99936a26327c1480a8f28fb445db20278b4c6
Connor <connorbp@hotmail.com>
2021-04-24 12:32:00 -0700

    map movement

---
+++ Assets/Scripts/World/MapManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public int ViewDistance;
+    public GameObject[] MapBlocks;
+
+    private Dictionary<Vector3, GameObject> _map = new Dictionary<Vector3, GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GenerateMap(ViewDistance));
+    }
+
+    public IEnumerator GenerateMap(int mapSize)
+    {
+        if (_map.Count > 0) _map.Clear();
+
+        // update this random logic
+        for (int z=0; z<mapSize; z++)
+        {
+            for (int x=0; x<mapSize; x++)
+            {
+                AddPosition(new Vector3(x, 0, z), Instantiate(GetBlockFromList()));
+                yield return null;
+            }
+        }
+    }
+
+    private GameObject GetBlockFromList()
+    {
+        return MapBlocks[Random.Range(0, MapBlocks.Length)];
+    }
+
+    /// <summary>
+    /// Tries to get the block on the map, returns false if doesnt exist.
+    /// </summary>
+    /// <param name="position"></param>
+    /// <param name="block"></param>
+    /// <returns></returns>
+    public bool TryGetPosition(Vector3 position, out GameObject block)
+    {
+        if (_map.TryGetValue(position, out block))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Adds new position or updates existing one.
+    /// </summary>
+    /// <param name="position"></param>
+    /// <param name="block"></param>
+    public void AddPosition(Vector3 position, GameObject block)
+    {
+        if (_map.ContainsKey(position))
+        {
+            _map[position] = block;
+        }
+        else
+        {
+            _map.Add(position, block);
+        }
+
+        block.transform.position = position;
+        block.transform.parent = transform;
+    }
+
+    /// <summary>
+    /// Removes position from map if it exists.
+    /// </summary>
+    /// <param name="position"></param>
+    public void RemovePosition(Vector3 position)
+    {
+        if (_map.ContainsKey(position))
+        {
+            _map.Remove(position);
+        }
+    }
+}
---
+++ Assets/Scripts/World/MapMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMovement : MonoBehaviour
+{
+    public GameObject Player;
+
+    private GameObject _hoveredObject;
+    private Ray _ray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.W)) MovePlayer(transform.forward + transform.position);
+        ChooseMovement();
+    }
+
+    //update to check if valid movement
+    public void ChooseMovement()
+    {
+        _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(_ray, out RaycastHit hit, 100))
+        {
+            if (hit.transform.CompareTag("Block"))
+            {
+                if (hit.transform.gameObject != _hoveredObject)
+                {
+                    _hoveredObject?.GetComponent<Renderer>().material.SetInt("_IsHovered", 0);
+                }
+
+                _hoveredObject = hit.transform.gameObject;
+                _hoveredObject.GetComponent<Renderer>().material.SetInt("_IsHovered", 1);
+            }
+        }
+        else
+        {
+            _hoveredObject?.GetComponent<Renderer>().material.SetInt("_IsHovered", 0);
+        }
+
+        if (Input.GetMouseButtonDown(0) && _hoveredObject.CompareTag("Block"))
+        {
+            MovePlayer(new Vector3(_hoveredObject.transform.position.x, 1, _hoveredObject.transform.position.z));
+        }
+    }
+
+    public void MovePlayer(Vector3 newPosition)
+    {
+        Vector3 previousPosition = Player.transform.position;
+        Player.transform.position = newPosition;
+        UpdateSurroundings(CalculateDirection(previousPosition, Player.transform.position));
+    }
+
+    private Direction CalculateDirection(Vector3 previousPosition, Vector3 currentPosition)
+    {
+        Vector3 direction = previousPosition - currentPosition;
+        print(direction);
+        return Direction.Forward;
+    }
+
+    public void UpdateSurroundings(Direction direction)
+    {
+
+    }
+}
+
+public enum Direction
+{
+    Forward,
+    Backward,
+    Left,
+    Right
+}
